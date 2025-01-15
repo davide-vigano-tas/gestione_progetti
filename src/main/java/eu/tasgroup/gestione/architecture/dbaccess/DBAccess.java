4b92de801a52ba1a273f0af99b5847a096b1cf93
davide-vigano-tas <davide.vigano@tasgroup.eu>
2025-01-15 15:27:40 +0100

    Fix DBACcess

--- src/main/java/eu/tasgroup/gestione/architecture/dbaccess/DBAccess.java
+++ src/main/java/eu/tasgroup/gestione/architecture/dbaccess/DBAccess.java
@@ -7,24 +7,33 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import eu.tasgroup.gestione.architetture.dao.DAOException;
+
 public class DBAccess {
 
 	private static Connection conn;
 	private static DataSource dataSource;
-	
-	
-	public static synchronized Connection getConnection() throws NamingException, SQLException {
-		//JNDI, prendiamo i parametri dalla configurazione wildfly
-		InitialContext contesto = new InitialContext();
-		//Legge string jndi
-		dataSource = (DataSource) contesto.lookup("java:/MySQLDS");
-		conn = dataSource.getConnection();
-		return conn;
+
+	public static synchronized Connection getConnection() throws NamingException, DAOException {
+		try {
+			// JNDI, prendiamo i parametri dalla configurazione wildfly
+			InitialContext contesto = new InitialContext();
+			// Legge string jndi
+			dataSource = (DataSource) contesto.lookup("java:/MySQLDS");
+			conn = dataSource.getConnection();
+			return conn;
+		} catch (SQLException sql) {
+			throw new DAOException(sql);
+		}
 	}
-	
-	public static void closeConnection() throws SQLException {
-		if(conn!=null) {
-			conn.close();
+
+	public static void closeConnection() throws DAOException {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException sql) {
+			throw new DAOException(sql);
 		}
 	}
 }
