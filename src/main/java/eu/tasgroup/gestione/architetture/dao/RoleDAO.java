fb1a30e46fb4951f031c4456a9513f7f1a07a790
samuel-mastrelli-tas <samuel.mastrelli@tasgroup.eu>
2025-01-15 18:03:21 +0100

    UserDAO and RoleDAO

---
+++ src/main/java/eu/tasgroup/gestione/architetture/dao/RoleDAO.java
@@ -0,0 +1,117 @@
+package eu.tasgroup.gestione.architetture.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import eu.tasgroup.gestione.businesscomponent.enumerated.Ruoli;
+import eu.tasgroup.gestione.businesscomponent.model.Role;
+
+public class RoleDAO extends DAOAdapter<Role> implements DAOConstants {
+
+	private RoleDAO() {}
+	
+	public static RoleDAO getFactory() {
+		return new RoleDAO();
+	}
+
+	@Override
+	public void create(Connection conn, Role entity) throws DAOException {
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(INSERT_USERS_ROLES);
+			ps.setLong(1, entity.getIdUser());
+			ps.setString(2, entity.getRole().name());
+
+			ps.execute();
+			conn.commit();
+			
+
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+
+	@Override
+	public void update(Connection conn, Role entity) throws DAOException {
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(UPDATE_USER_ROLES);
+			ps.setString(1, entity.getRole().name());
+			ps.setLong(2, entity.getIdUser());
+			ps.setString(3, entity.getRole().name());
+			
+
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+	
+	public Ruoli[] getByUserId(Connection conn, long id_user) throws DAOException {
+		Ruoli[] roles = null;
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(SELECT_USER_ROLES_BY_USER_ID,
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			ps.setLong(1, id_user);
+			ResultSet rs = ps.executeQuery();
+			rs.last();
+			roles = new Ruoli[rs.getRow()];
+			rs.beforeFirst();
+			for (int i = 0; rs.next(); i++) {
+		
+				
+				roles[i] = Ruoli.valueOf(rs.getString(1));
+				
+			} 
+			
+			return roles;
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+	
+	
+	public Ruoli[] getByUsername(Connection conn, String username) throws DAOException {
+		Ruoli[] roles = null;
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(SELECT_USER_ROLES_BY_USERNAME,
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			ps.setString(1, username);
+			ResultSet rs = ps.executeQuery();
+			rs.last();
+			roles = new Ruoli[rs.getRow()];
+			rs.beforeFirst();
+			for (int i = 0; rs.next(); i++) {
+		
+				
+				roles[i] = Ruoli.valueOf(rs.getString(1));
+				
+			} 
+			
+			return roles;
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+	
+	
+	@Override
+	public void delete(Connection conn, long id) throws DAOException {
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(DELETE_USER_ROLE);
+			ps.setLong(1, id);
+			ps.execute();
+			
+
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+	
+}
---
+++ src/main/java/eu/tasgroup/gestione/architetture/dao/UserDAO.java
@@ -0,0 +1,224 @@
+package eu.tasgroup.gestione.architetture.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import eu.tasgroup.gestione.businesscomponent.enumerated.Skills;
+import eu.tasgroup.gestione.businesscomponent.model.User;
+import eu.tasgroup.gestione.businesscomponent.security.Algoritmo;
+
+public class UserDAO extends DAOAdapter<User> implements DAOConstants {
+	
+	private UserDAO() {}
+	
+	public static UserDAO getFactory() {
+		return new UserDAO();
+	}
+
+	@Override
+	public void create(Connection conn, User entity) throws DAOException {
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(INSERT_USERS);
+			ps.setString(1, entity.getNome());
+			ps.setString(2, entity.getCognome());
+			ps.setString(3, entity.getUsername());
+			ps.setString(4, Algoritmo.converti(entity.getPassword()));
+			ps.setString(5, entity.getEmail());
+
+			ps.execute();
+			
+
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+
+	@Override
+	public void update(Connection conn, User entity) throws DAOException {
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(UPDATE_USER);
+			ps.setString(1, entity.getNome());
+			ps.setString(2,  entity.getCognome());
+			ps.setString(3, entity.getUsername());
+			ps.setString(4, entity.getEmail());
+			ps.setInt(5, entity.getTentativiFalliti());
+			ps.setBoolean(6, entity.isLocked());
+			ps.setLong(7, entity.getId());
+			
+			
+
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+	
+
+	@Override
+	public User getById(Connection conn, long id) throws DAOException {
+		User user = null;
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(SELECT_USER_ID);
+			ps.setLong(1, id);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				user = new User();
+				user.setId(rs.getLong(1));
+				user.setNome(rs.getString(2));
+				user.setCognome(rs.getString(3));
+				user.setUsername(rs.getString(4));
+				user.setPassword(rs.getString(5));
+				user.setEmail(rs.getString(6));
+				user.setTentativiFalliti(rs.getInt(7));
+				user.setLocked(rs.getBoolean(8));
+				user.setDataCreazione(rs.getTimestamp(9).toLocalDateTime());
+				
+			} 
+			
+			return user;
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+	
+	
+	public User getByUsername(Connection conn, String username) throws DAOException {
+		User user = null;
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(SELECT_USER_USERNAME);
+			ps.setString(1, username);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				user = new User();
+				user.setId(rs.getLong(1));
+				user.setNome(rs.getString(2));
+				user.setCognome(rs.getString(3));
+				user.setUsername(rs.getString(4));
+				user.setPassword(rs.getString(5));
+				user.setEmail(rs.getString(6));
+				user.setTentativiFalliti(rs.getInt(7));
+				user.setLocked(rs.getBoolean(8));
+				user.setDataCreazione(rs.getTimestamp(9).toLocalDateTime());
+				
+			} 
+			
+			return user;
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+	
+	public User getByEmail(Connection conn, String email) throws DAOException {
+		User user = null;
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(SELECT_USER_EMAIL);
+			ps.setString(1, email);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				user = new User();
+				user.setId(rs.getLong(1));
+				user.setNome(rs.getString(2));
+				user.setCognome(rs.getString(3));
+				user.setUsername(rs.getString(4));
+				user.setPassword(rs.getString(5));
+				user.setEmail(rs.getString(6));
+				user.setTentativiFalliti(rs.getInt(7));
+				user.setLocked(rs.getBoolean(8));
+				user.setDataCreazione(rs.getTimestamp(9).toLocalDateTime());
+				
+			} 
+			
+			return user;
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+
+	@Override
+	public User[] getAll(Connection conn) throws DAOException {
+		User[] users = null;
+
+		try {
+			Statement stmt = conn.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			
+			ResultSet rs = stmt.executeQuery(SELECT_USERS);
+			rs.last();
+			users = new User[rs.getRow()];
+			rs.beforeFirst();
+			for(int i = 0; rs.next(); i++) {
+				User user = new User();
+				user.setId(rs.getLong(1));
+				user.setNome(rs.getString(2));
+				user.setCognome(rs.getString(3));
+				user.setUsername(rs.getString(4));
+				user.setPassword(rs.getString(5));
+				user.setEmail(rs.getString(6));
+				user.setTentativiFalliti(rs.getInt(7));
+				user.setLocked(rs.getBoolean(8));
+				user.setDataCreazione(rs.getTimestamp(9).toLocalDateTime());
+				users[i] = user;
+			} 
+			return users;
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+	
+	public User[] getBySkill(Connection conn, Skills skill) throws DAOException {
+		User[] users = null;
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(SELECT_USERS_BY_SKILL,
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			ps.setString(1, skill.name());
+			ResultSet rs = ps.executeQuery();
+			rs.last();
+			users = new User[rs.getRow()];
+			rs.beforeFirst();
+			for (int i = 0; rs.next(); i++) {
+				User user  = new User();
+				user.setId(rs.getLong(1));
+				user.setNome(rs.getString(2));
+				user.setCognome(rs.getString(3));
+				user.setUsername(rs.getString(4));
+				user.setPassword(rs.getString(5));
+				user.setEmail(rs.getString(6));
+				user.setTentativiFalliti(rs.getInt(7));
+				user.setLocked(rs.getBoolean(8));
+				user.setDataCreazione(rs.getTimestamp(9).toLocalDateTime());
+				
+				users[i] = user;
+				
+			} 
+			
+			return users;
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+
+	}
+	
+	@Override
+	public void delete(Connection conn, long id) throws DAOException {
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(DELETE_USER);
+			ps.setLong(1, id);
+			ps.execute();
+			
+
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+}
