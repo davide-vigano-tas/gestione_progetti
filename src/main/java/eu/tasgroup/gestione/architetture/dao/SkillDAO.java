6d23d191e25cf8e9e04d97573243b6f5c5aebc69
samuel-mastrelli-tas <samuel.mastrelli@tasgroup.eu>
2025-01-16 11:07:01 +0100

    SkillDAO

---
+++ src/main/java/eu/tasgroup/gestione/architetture/dao/SkillDAO.java
@@ -0,0 +1,168 @@
+package eu.tasgroup.gestione.architetture.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import eu.tasgroup.gestione.businesscomponent.enumerated.Skills;
+import eu.tasgroup.gestione.businesscomponent.model.Skill;
+
+public class SkillDAO extends DAOAdapter<Skill> implements DAOConstants {
+
+	private SkillDAO() {}
+	
+	public static SkillDAO getFactory() {
+		return new SkillDAO();
+	}
+
+	@Override
+	public void create(Connection conn, Skill entity) throws DAOException {
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(INSERT_SKILL);
+			ps.setString(1, entity.getTipo().name());
+
+			ps.execute();
+			
+
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+
+
+	
+	
+	
+	
+	@Override
+	public Skill getById(Connection conn, long id) throws DAOException {
+		Skill skill = null;
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(SELECT_SKILL);
+			ps.setLong(1, id);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				skill = new Skill();
+				skill.setId(rs.getLong(1));
+				skill.setTipo(Skills.valueOf(rs.getString(2)));
+				
+			} 
+			
+			return skill;
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+
+	@Override
+	public Skill[] getAll(Connection conn) throws DAOException {
+		Skill[] skills = null;
+
+		try {
+			Statement stmt = conn.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			
+			ResultSet rs = stmt.executeQuery(SELECT_SKILLS);
+			rs.last();
+			skills = new Skill[rs.getRow()];
+			rs.beforeFirst();
+			for(int i = 0; rs.next(); i++) {
+				Skill skill = new Skill();
+				skill.setId(rs.getLong(1));
+				skill.setTipo(Skills.valueOf(rs.getString(2)));
+				skills[i] = skill;
+			} 
+			return skills;
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+
+	public Skill[] getByTipo(Connection conn, Skills skilltype) throws DAOException {
+		Skill[] skills = null;
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(SELECT_SKILLS_BY_TIPO,
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			ps.setString(1, skilltype.name());
+			ResultSet rs = ps.executeQuery();
+			rs.last();
+			skills = new Skill[rs.getRow()];
+			rs.beforeFirst();
+			for (int i = 0; rs.next(); i++) {
+				Skill skill = new Skill();
+				skill.setId(rs.getLong(1));
+				skill.setTipo(Skills.valueOf(rs.getString(2)));
+				skills[i] = skill;
+				
+				
+			} 
+			
+			return skills;
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+
+	}
+	
+	public Skill[] getByUser(Connection conn, long user) throws DAOException {
+		Skill[] skills = null;
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(SELECT_SKILLS_BY_USER,
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			ps.setLong(1, user);
+			ResultSet rs = ps.executeQuery();
+			rs.last();
+			skills = new Skill[rs.getRow()];
+			rs.beforeFirst();
+			for (int i = 0; rs.next(); i++) {
+				Skill skill = new Skill();
+				skill.setId(rs.getLong(1));
+				skill.setTipo(Skills.valueOf(rs.getString(2)));
+				skills[i] = skill;
+				
+				
+			} 
+			
+			return skills;
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+
+	}
+	@Override
+	public void delete(Connection conn, long id) throws DAOException {
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(DELETE_SKILL);
+			ps.setLong(1, id);
+			ps.execute();
+			
+
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+	
+	public void deleteByTipo(Connection conn, Skills skill) throws DAOException {
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(DELETE_SKILLS_BY_TIPO);
+			ps.setString(1, skill.name());
+			ps.execute();
+			
+
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+	
+}
---
+++ src/test/java/test/eu/tasgroup/gestione/architetture/dao/SkillDAOTest.java
@@ -0,0 +1,151 @@
+package test.eu.tasgroup.gestione.architetture.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+
+import javax.naming.NamingException;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import eu.tasgroup.gestione.architecture.dbaccess.DBAccess;
+import eu.tasgroup.gestione.architetture.dao.SkillDAO;
+import eu.tasgroup.gestione.architetture.dao.UserDAO;
+import eu.tasgroup.gestione.businesscomponent.enumerated.Skills;
+import eu.tasgroup.gestione.businesscomponent.model.Skill;
+import eu.tasgroup.gestione.businesscomponent.model.User;
+import test.eu.tasgroup.gestione.DBAccessContext;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class SkillDAOTest {
+	
+	private Connection conn;
+	private static Skill skill;
+	private static User user1;
+	
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		
+		user1 = new User();
+		user1.setNome("Sam");
+		user1.setCognome("Mast");
+		user1.setUsername("sammast");
+		user1.setPassword("pass");
+		user1.setEmail("sam@gmail.com");
+		
+		skill = new Skill();
+		skill.setTipo(Skills.JAVA17);
+	}
+
+	@BeforeEach
+    void setUp() throws NamingException, ClassNotFoundException {
+    	DBAccessContext.setDBAccessContext();
+    }
+
+	@Test
+	@Order(1)
+	void testCreateAndGetByTipo() {
+		try {
+			conn = DBAccess.getConnection();
+			UserDAO.getFactory().create(conn, user1);
+			user1 = UserDAO.getFactory().getByUsername(conn, user1.getUsername());
+			
+			Skill[] skills1 = SkillDAO.getFactory().getByTipo(conn, Skills.JAVA17);
+			
+			SkillDAO.getFactory().create(conn, skill);
+			
+			Skill[] skills2 = SkillDAO.getFactory().getByTipo(conn, Skills.JAVA17);
+			
+			assertTrue(skills1.length == skills2.length-1, "Skill non aggiunto correttamente");
+			
+			skill = skills2[0];
+			
+			assertNotNull(skill, "Skill non deve essere null");
+			
+			
+			DBAccess.closeConnection(conn);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Failed: "+e.getMessage());
+		}
+	}
+	
+	@Test
+	@Order(2)
+	void testGetById() {
+		try {
+			conn = DBAccess.getConnection();
+			
+			skill = SkillDAO.getFactory().getById(conn, skill.getId());
+			
+			assertNotNull(skill, "Skill non deve essere null");
+			
+			
+			DBAccess.closeConnection(conn);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Failed: "+e.getMessage());
+		}
+	}
+	
+	@Test
+	@Order(3)
+	void testGetAll() {
+		try {
+			conn = DBAccess.getConnection();
+			
+			Skill[] skills2 = SkillDAO.getFactory().getAll(conn);
+			
+			assertTrue(skills2.length >= 1, "Skill non aggiunto correttamente");
+			
+			
+			DBAccess.closeConnection(conn);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Failed: "+e.getMessage());
+		}
+	}
+	
+	@Test
+	@Order(4)
+	void testGetByUser() {
+		try {
+			conn = DBAccess.getConnection();
+
+			
+			assertNotNull(skill, "Skill non deve essere null");
+			
+			
+			DBAccess.closeConnection(conn);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Failed: "+e.getMessage());
+		}
+	}
+	
+	@Test
+	@Order(5)
+	void testDelete() {
+		try {
+			conn = DBAccess.getConnection();
+			
+			SkillDAO.getFactory().delete(conn, skill.getId());
+			UserDAO.getFactory().delete(conn, user1.getId());
+			skill = SkillDAO.getFactory().getById(conn, skill.getId());
+			
+			assertNull(skill, "Skill non deve essere null");
+			
+			
+			DBAccess.closeConnection(conn);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Failed: "+e.getMessage());
+		}
+	}
+
+}
