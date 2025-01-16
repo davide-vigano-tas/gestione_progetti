436648a497d110793f982374591c6a7970a3dc40
samuel-mastrelli-tas <samuel.mastrelli@tasgroup.eu>
2025-01-16 10:26:36 +0100

    PaymentDAO and PaymentDAOTest

---
+++ src/main/java/eu/tasgroup/gestione/architetture/dao/PaymentDAO.java
@@ -0,0 +1,162 @@
+package eu.tasgroup.gestione.architetture.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import eu.tasgroup.gestione.businesscomponent.model.Payment;
+import eu.tasgroup.gestione.businesscomponent.model.Project;
+import eu.tasgroup.gestione.businesscomponent.model.User;
+
+public class PaymentDAO extends DAOAdapter<Payment> implements DAOConstants {
+	
+	private PaymentDAO() {}
+	
+	public static PaymentDAO getFactory() {
+		return new PaymentDAO();
+	}
+
+	@Override
+	public void create(Connection conn, Payment entity) throws DAOException {
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(INSERT_PAYMENT);
+			ps.setLong(1, entity.getId_progetto());
+			ps.setDouble(2, entity.getCifra());
+
+			ps.execute();
+			
+
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+
+
+	
+
+	@Override
+	public Payment getById(Connection conn, long id) throws DAOException {
+		Payment payment = null;
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(SELECT_PAYMENT_BY_ID);
+			ps.setLong(1, id);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				payment = new Payment();
+				payment.setId(rs.getLong(1));
+				payment.setId_progetto(rs.getLong(2));
+				payment.setCifra(rs.getDouble(3));
+				
+			} 
+			
+			return payment;
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+	
+
+
+
+	@Override
+	public Payment[] getAll(Connection conn) throws DAOException {
+		Payment[] payments = null;
+
+		try {
+			Statement stmt = conn.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			
+			ResultSet rs = stmt.executeQuery(SELECT_PAYMENTS);
+			rs.last();
+			payments = new Payment[rs.getRow()];
+			rs.beforeFirst();
+			for(int i = 0; rs.next(); i++) {
+				Payment payment = new Payment();
+				payment.setId(rs.getLong(1));
+				payment.setId_progetto(rs.getLong(2));
+				payment.setCifra(rs.getDouble(3));
+				payments[i] = payment;
+			} 
+			return payments;
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+	
+	public Payment[] getByUser(Connection conn, User user) throws DAOException {
+		Payment[] payments = null;
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(SELECT_PAYMENTS_BY_USER,
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			ps.setLong(1, user.getId());
+			ResultSet rs = ps.executeQuery();
+			rs.last();
+			payments = new Payment[rs.getRow()];
+			rs.beforeFirst();
+			for (int i = 0; rs.next(); i++) {
+				Payment payment = new Payment();
+				payment.setId(rs.getLong(1));
+				payment.setId_progetto(rs.getLong(2));
+				payment.setCifra(rs.getDouble(3));
+				payments[i] = payment;
+				
+				
+			} 
+			
+			return payments;
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+
+	}
+	
+	public Payment[] getByProject(Connection conn, Project project) throws DAOException {
+		Payment[] payments = null;
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(SELECT_PAYMENTS_BY_PROJECT,
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			ps.setLong(1, project.getId());
+			ResultSet rs = ps.executeQuery();
+			rs.last();
+			payments = new Payment[rs.getRow()];
+			rs.beforeFirst();
+			for (int i = 0; rs.next(); i++) {
+				Payment payment = new Payment();
+				payment.setId(rs.getLong(1));
+				payment.setId_progetto(rs.getLong(2));
+				payment.setCifra(rs.getDouble(3));
+				payments[i] = payment;
+				
+				
+			} 
+			
+			return payments;
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+
+	}
+	
+	@Override
+	public void delete(Connection conn, long id) throws DAOException {
+		PreparedStatement ps;
+		try {
+			ps = conn.prepareStatement(DELETE_PAYMENT);
+			ps.setLong(1, id);
+			ps.execute();
+			
+
+		} catch (SQLException e) {
+			throw new DAOException(e);
+		}
+	}
+}
---
+++ src/test/java/test/eu/tasgroup/gestione/architetture/dao/PaymentDAOTest.java
@@ -0,0 +1,190 @@
+package test.eu.tasgroup.gestione.architetture.dao;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.sql.Connection;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+import javax.naming.NamingException;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import eu.tasgroup.gestione.architecture.dbaccess.DBAccess;
+import eu.tasgroup.gestione.architetture.dao.PaymentDAO;
+import eu.tasgroup.gestione.architetture.dao.ProjectDAO;
+import eu.tasgroup.gestione.architetture.dao.UserDAO;
+import eu.tasgroup.gestione.businesscomponent.model.Payment;
+import eu.tasgroup.gestione.businesscomponent.model.Project;
+import eu.tasgroup.gestione.businesscomponent.model.User;
+import test.eu.tasgroup.gestione.DBAccessContext;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class PaymentDAOTest {
+	
+	private Connection conn;
+	private static User cliente;
+	private static User projectmanager;
+	private static Project project;
+	private static Payment payment;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		
+		cliente = new User();
+		cliente.setNome("Sam");
+		cliente.setCognome("Mast");
+		cliente.setUsername("sammast");
+		cliente.setPassword("pass");
+		cliente.setEmail("sam@gmail.com");
+		
+		
+		projectmanager = new User();
+		projectmanager.setNome("Rob");
+		projectmanager.setCognome("Bru");
+		projectmanager.setUsername("robbru");
+		projectmanager.setPassword("pass");
+		projectmanager.setEmail("rob@gmail.com");
+		
+		
+		project = new Project();
+		project.setNomeProgetto("Progetto");
+		project.setBudget(300000);
+		project.setCostoProgetto(500000);
+		project.setDescrizione("Descrizione");
+		project.setDataInizio(new Date());
+		project.setDataFine(new Date(LocalDateTime.now().plusMonths(6).getSecond()*1000));
+		
+		payment = new Payment();
+		payment.setCifra(300000);
+	}
+
+	@BeforeEach
+    void setUp() throws NamingException, ClassNotFoundException {
+    	DBAccessContext.setDBAccessContext();
+    }
+
+	@Test
+	@Order(1)
+	void testCreateAndGetByUser() {
+		try {
+			conn = DBAccess.getConnection();
+			UserDAO.getFactory().create(conn, cliente);
+			UserDAO.getFactory().create(conn, projectmanager);
+			
+			cliente = UserDAO.getFactory().getByUsername(conn, cliente.getUsername());
+			projectmanager = UserDAO.getFactory().getByUsername(conn, projectmanager.getUsername());
+			
+			project.setIdCliente(cliente.getId());
+			project.setIdResponsabile(projectmanager.getId());
+			
+			ProjectDAO.getFactory().create(conn, project);
+			List<Project> pr = ProjectDAO.getFactory().getListProjectByCliente(conn, cliente.getId());
+			project = pr.get(0);
+			
+			payment.setId_progetto(project.getId());
+			PaymentDAO.getFactory().create(conn, payment);
+			
+			Payment[] ps = PaymentDAO.getFactory().getByUser(conn, cliente);
+			assertNotNull(ps, "Elenco payments non deve essere null");
+			assertTrue(ps.length == 1, "Deve contenere un elemento");
+			payment = ps[0];
+			assertNotNull(payment, "L'elemento non deve essere null");
+			DBAccess.closeConnection(conn);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Failed "+e.getMessage());
+		}
+	}
+	
+	@Test
+	@Order(2)
+	void testGetByProject() {
+		try {
+		
+			conn = DBAccess.getConnection();
+			Payment[] ps = PaymentDAO.getFactory().getByProject(conn, project);
+			assertNotNull(ps, "Elenco payments non deve essere null");
+			assertTrue(ps.length == 1, "Deve contenere un elemento");
+			payment = ps[0];
+			assertNotNull(payment, "L'elemento non deve essere null");
+			DBAccess.closeConnection(conn);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Failed "+e.getMessage());
+		}
+	}
+	
+	@Test
+	@Order(3)
+	void testGetById() {
+		try {
+		
+			conn = DBAccess.getConnection();
+			payment = PaymentDAO.getFactory().getById(conn, payment.getId());
+
+			assertNotNull(payment, "L'elemento non deve essere null");
+			DBAccess.closeConnection(conn);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Failed "+e.getMessage());
+		}
+	}
+	
+	@Test
+	@Order(3)
+	void testGetAll() {
+		try {
+		
+			conn = DBAccess.getConnection();
+			Payment[] ps = PaymentDAO.getFactory().getAll(conn);
+			assertNotNull(ps, "Elenco payments non deve essere null");
+			assertTrue(ps.length >= 1, "Deve contenere almeno un elemento");
+			DBAccess.closeConnection(conn);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Failed "+e.getMessage());
+		}
+	}
+
+	
+	
+	@Test
+	@Order(4)
+	void testDelete() {
+		try {
+			conn = DBAccess.getConnection();
+			
+			PaymentDAO.getFactory().delete(conn, payment.getId());
+			payment = PaymentDAO.getFactory().getById(conn, payment.getId());
+			assertNull(payment, "Il pagamento non è stato eliminato con successo");
+			
+			ProjectDAO.getFactory().delete(conn, project.getId());
+			project = ProjectDAO.getFactory().getById(conn, project.getId());
+			assertNull(project, "Il progetto non è stato eliminato con successo");
+			
+			UserDAO.getFactory().delete(conn, cliente.getId());
+			UserDAO.getFactory().delete(conn, projectmanager.getId());
+			
+			cliente = UserDAO.getFactory().getByEmail(conn,cliente.getEmail());
+			projectmanager = UserDAO.getFactory().getByEmail(conn, projectmanager.getEmail());
+			assertNull(cliente, "cliente dovrebbe essere null");
+			assertNull(projectmanager, "projectmanager dovrebbe essere null");
+			
+			DBAccess.closeConnection(conn);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Failed: "+e.getMessage());
+		}
+	}
+}
